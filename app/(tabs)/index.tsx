f21efd2580fcda956baeb6abefac59006623c5a0
Yewon <ymh20804@naver.com>
2026-07-19 11:31:25 +0900

    feat: Inter폰트 적용

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,8 +5,11 @@ import { cn } from "@/lib/utils";
 export default function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className={cn("text-lg font-bold text-red-500", "underline")}>
-        NativeWind test
+      <Text
+        className={cn("text-lg text-black")}
+        style={{ fontFamily: "Inter_600SemiBold" }}
+      >
+        비밀번호 찾기
       </Text>
     </View>
   );
